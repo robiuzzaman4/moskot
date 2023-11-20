af4dd18e209821cdc9c1889a6e00f653a3f1c35c
ruhan <robiuzzaman4@gmail.com>
2023-11-20 15:44:59 +0600

    design login and sign up page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,41 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Login = () => {
   return (
     <section className="login">
-      Login
+      <div className="login-card">
+        <Image src="/team-logo.svg" alt="Team Logo" height={80} width={80} />
+
+        <h2 className="font-lg login-title">
+          Welcome to <span>Back!</span>
+        </h2>
+
+        <form className="form">
+          <input
+            className="input"
+            type="email"
+            name="email"
+            placeholder="Email Address"
+          />
+          <input
+            className="input"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <p className="forgot-pass">Forgot password?</p>
+          <button type="submit" className="btn-indigo-solid">
+            Continue
+          </button>
+          <p className="signup-cta">
+            Don&apos;t have an account?
+            <Link href="/signup" className="signup-link">
+              Sign up
+            </Link>
+          </p>
+        </form>
+      </div>
     </section>
   );
 };
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,42 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Signup = () => {
-  return <section className="signup">Sign up</section>;
+  return (
+    <section className="signup">
+      <div className="signup-card">
+        <Image src="/team-logo.svg" alt="Team Logo" height={80} width={80} />
+
+        <h2 className="font-lg signup-title">
+          Sign Up Now
+        </h2>
+
+        <form className="form">
+          <input
+            className="input"
+            type="email"
+            name="email"
+            placeholder="Email Address"
+          />
+          <input
+            className="input"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <button type="submit" className="btn-indigo-solid">
+            Continue
+          </button>
+          <p className="login-cta">
+            Already have an account?
+            <Link href="/login" className="login-link">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Signup;
